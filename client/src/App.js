dad102179b97c073e6666e9dc532edf1fd40e61a
Aleksey Belchenkov <u608110@gmail.com>
2019-04-13 23:55:54 +0300

    React & Redux Auth State

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,17 @@ import ItemModal from './components/ItemModal';
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from "./actions/authActions";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
-  render() {
+    componentDidMount() {
+        store.dispatch(loadUser());
+    }
+
+    render() {
     return (
         <Provider store={store}>
             <div className="App">
